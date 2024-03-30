e0c094976f5b77c07dd8c2f057060846f3bf0938
Jacob Michael <thomasjmichael@outlook.com>
2024-03-30 18:20:35 -0500

    Time Tracking Controller

---
+++ TCMS.API/Controllers/TimeTrackingController.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using TCMS.Common.DTOs.Financial;
+using TCMS.Common.Operations;
+using TCMS.Services.Interfaces;
+
+namespace TCMS.API.Controllers
+{
+    [Route("api/time-tracking")]
+    [ApiController]
+    public class TimeTrackingController : ControllerBase
+    {
+        private readonly ITimeTrackingService _timeTrackingService;
+
+        public TimeTrackingController(ITimeTrackingService timeTrackingService)
+        {
+            _timeTrackingService = timeTrackingService;
+        }
+
+        // Clock in
+        [HttpPost("clock-in")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OperationResult))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<OperationResult>> ClockIn()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = await _timeTrackingService.ClockInAsync(userId);
+            return result.IsSuccessful ? Ok(result) : BadRequest(result);
+        }
+
+        // Clock out
+        [HttpPost("clock-out")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OperationResult))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<OperationResult>> ClockOut()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = await _timeTrackingService.ClockOutAsync(userId);
+            return result.IsSuccessful ? Ok(result) : BadRequest(result);
+        }
+
+        // Get all time sheets
+        [HttpGet("all")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OperationResult<IEnumerable<TimesheetDto>>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<OperationResult<IEnumerable<TimesheetDto>>>> GetAllTimeSheets(GetTimeSheetsDto requestDto)
+        {
+            var result = await _timeTrackingService.GetTimesheetsForPeriodAsync(requestDto.EmployeeId, requestDto.StartDate, requestDto.EndDate);
+            return result.IsSuccessful ? Ok(result) : BadRequest(result);
+        }
+    }
+}
---
+++ TCMS.Common/DTOs/Financial/GetTimeSheetsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCMS.Common.DTOs.Financial
+{
+    public class GetTimeSheetsDto
+    {
+        [Required]
+        public string EmployeeId { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+    }
+}
