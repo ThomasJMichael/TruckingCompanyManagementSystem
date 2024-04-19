a4f8cbdc22e5eca89b80ea51ade2cb5781f77b03
BJ Bradham <whb0006@uah.edi>
2024-04-19 14:28:29 -0500

    Added Display and some Time Logic

--- TCMS.GUI/ViewModels/TimeClockViewModel.cs
+++ TCMS.GUI/ViewModels/TimeClockViewModel.cs
@@ -1,13 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TCMS.GUI.ViewModels
 {
-    public class TimeClockViewModel
-    {
+        public class TimeClockViewModel : INotifyPropertyChanged
+        {
+            private ObservableCollection<TimeRecord> _timeRecords;
+
+            public ObservableCollection<TimeRecord> TimeRecords
+            {
+                get => _timeRecords;
+                set
+                {
+                    _timeRecords = value;
+                    OnPropertyChanged(nameof(TimeRecords));
+                }
+            }
+
+            public TimeClockViewModel()
+            {
+                TimeRecords = new ObservableCollection<TimeRecord>
+            {
+                // Example data
+                new TimeRecord { ClockInTime = DateTime.Now.AddHours(-1), ClockOutTime = DateTime.Now, TotalHours = 1 }
+            };
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public class TimeRecord
+        {
+            public DateTime ClockInTime { get; set; }
+            public DateTime ClockOutTime { get; set; }
+            public double TotalHours { get; set; }
+        }
     }
-}
+
+
 
